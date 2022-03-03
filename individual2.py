e9b1d0627656c5da987f80e76d57103e4191442a
student-09-525 <student-09-525@ncfu.net>
2022-03-03 12:34:36 +0300

    lab-5 shebanq and if __main__

--- individual2.py
+++ individual2.py
@@ -1,6 +1,12 @@
-x = float(input("Введите координату x: "))
-y = float(input("Введите координату y: "))
-if (x*x + y*y <=1) and (x*x + y*y >= 0.25):
-    print("Принадлежит")
-else:
-    print("Не принадлежит")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    x = float(input("Введите координату x: "))
+    y = float(input("Введите координату y: "))
+
+    if (x*x + y*y <=1) and (x*x + y*y >= 0.25):
+        print("Принадлежит")
+    else:
+        print("Не принадлежит")
--- individual3.py
+++ individual3.py
@@ -1,4 +1,11 @@
-ameba_cells = 1
-for i in range(6):
-    ameba_cells *= 2
-print(f"Будет {ameba_cells} клетки через 6 часов ")
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+
+    ameba_cells = 1
+
+    for i in range(6):
+        ameba_cells *= 2
+
+    print(f"Будет {ameba_cells} клетки через 6 часов ")
